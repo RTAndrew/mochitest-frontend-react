2351a4695fc4d63967470b15b1de52fec8260c9c
RTAndrew <anderson_rodax@hotmail.com>
2021-02-25 23:31:31 +0100

    refactor(card): add card link to navigate
    The user will be able to navigate to either the users page or the organizations page

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface CardProps {
   data: {
@@ -10,15 +11,22 @@ interface CardProps {
 }
 
 const Card = ({ data }: CardProps) => {
+
+  function getCardLink(): string {
+    if (data.type === 'Organization') return `/orgs/${data.login}`;
+
+    return `/users/${data.login}`;
+  }
+
   return (
-    <div className="card">
-      <img src={data.avatar_url} alt="#" className="card__avatar" />
+    <Link to={getCardLink()} className="card">
+      <img src={data.avatar_url} alt="" className="card__avatar" />
       <div className="card__body">
         <div className="card__header">{data.login}</div>
         <div className="card__name">{data.name}</div>
       </div>
       <div className="card__footer"></div>
-    </div>
+    </Link>
   );
 };
 
