6807dea937af35f478b4b29df1fbe9f338a0b26d
RTAndrew <anderson_rodax@hotmail.com>
2021-02-28 02:28:48 +0100

    test(searchResults): test useContext of the Container

---
+++ src/components/search-results/Container.test.tsx
@@ -0,0 +1,97 @@
+import { render, screen } from '@testing-library/react';
+import { StoreContext } from 'contexts';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Container from './Container';
+
+describe('Container Component', () => {
+  let loading: boolean = false;
+  let isError: boolean = false;
+  let setLoading = () => {};
+  let setIsError = () => {};
+  let query: string = '';
+  let setQuery = () => {};
+  let queryResult: any[] = [];
+  let setQueryResult = () => {};
+
+  let store = {
+    loading,
+    isError,
+    setLoading,
+    setIsError,
+    query,
+    setQuery,
+    queryResult,
+    setQueryResult,
+  };
+
+  beforeEach(() => {
+    store.loading = false;
+    store.isError = false;
+    store.query = '';
+    store.queryResult = [];
+  });
+
+  it('should render a loading spinner', () => {
+    store.loading = true;
+    render(
+      <StoreContext.Provider value={store}>
+        <Container />
+      </StoreContext.Provider>,
+    );
+
+    expect(document.getElementsByClassName('loadingSpinner').length).toBe(1);
+  });
+
+  it('should render an error message', () => {
+    store.isError = true;
+    render(
+      <StoreContext.Provider value={store}>
+        <Container />
+      </StoreContext.Provider>,
+    );
+
+    expect(screen.getByText(/we could not process your request/i)).toBeInTheDocument;
+  });
+
+  it('should render the results', () => {
+    store.queryResult = [
+      {
+        login: 'Anderson Rodax',
+        id: 7697157,
+        node_id: 'MDQ6VXNlcjc2OTcxNTc=',
+        avatar_url: 'https://avatars.githubusercontent.com/u/7697157?v=4',
+        gravatar_id: '',
+        url: 'https://api.github.com/users/ax',
+        html_url: 'https://github.com/ax',
+        followers_url: 'https://api.github.com/users/ax/followers',
+        following_url: 'https://api.github.com/users/ax/following{/other_user}',
+        gists_url: 'https://api.github.com/users/ax/gists{/gist_id}',
+        starred_url: 'https://api.github.com/users/ax/starred{/owner}{/repo}',
+        subscriptions_url: 'https://api.github.com/users/ax/subscriptions',
+        organizations_url: 'https://api.github.com/users/ax/orgs',
+        repos_url: 'https://api.github.com/users/ax/repos',
+        events_url: 'https://api.github.com/users/ax/events{/privacy}',
+        received_events_url: 'https://api.github.com/users/ax/received_events',
+        type: 'User',
+        site_admin: false,
+        score: 1.0,
+      },
+    ];
+
+    render(
+      <Router>
+        <Switch>
+          <StoreContext.Provider value={store}>
+            <Container />
+          </StoreContext.Provider>
+        </Switch>
+      </Router>,
+    );
+
+    expect(screen.findAllByText(/Anderson Rodax/i)).toBeInTheDocument;
+    expect(screen.findAllByText(/users (1)/i)).toBeInTheDocument;
+    expect(screen.findAllByText(/COMPANIES (0)/i)).toBeInTheDocument;
+  });
+});
