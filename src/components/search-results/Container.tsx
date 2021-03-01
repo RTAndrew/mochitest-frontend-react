bcba41caf641f85ce0c33341ba759a5eaf5a519d
RTAndrew <anderson_rodax@hotmail.com>
2021-03-01 05:02:41 +0100

    hotfix(responsiveness): add conditional rendering
    Because the responsiveness was done in CSS with display:none, still, the card component was still fetching from the API.
    
    With conditional rendering, the elements wont appear in the document, and, thus, preventing "ghost fetching."

--- src/components/search-results/Container.tsx
+++ src/components/search-results/Container.tsx
@@ -1,12 +1,22 @@
 import { Spin, Tabs } from 'antd';
 import { EmptyMessage, OrganizationList, UserList } from 'components';
 import { StoreContext } from 'contexts';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 const { TabPane } = Tabs;
 
 const SearchResultsContainer = () => {
   const { queryResult, loading, isError } = useContext(StoreContext);
+  const [size, setSize] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    const updateSize = () => setSize({ width: window.innerWidth, height: window.innerHeight });
+
+    window.addEventListener('resize', updateSize);
+    updateSize();
+
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
 
   function countUsers(): number {
     const filter = queryResult!.items.filter(result => result.type === 'User');
@@ -24,36 +34,38 @@ const SearchResultsContainer = () => {
 
   return (
     <div className="searchResults">
-      <div className="searchResults--mobile">
-        <Tabs defaultActiveKey="1">
-          <TabPane tab={`USERS (${countUsers()})`} key="1">
-            <UserList />
-          </TabPane>
-          <TabPane tab={`COMPANIES (${countOrganizations()})`} key="2">
-            <OrganizationList />
-          </TabPane>
-        </Tabs>
-      </div>
-
-      <div className="searchResults--desktop">
-        <div className="searchResults--desktop__list">
-          <div className="searchResults--desktop__items">
-            <div className="searchResults--desktop__header">{`USERS (${countUsers()})`}</div>
-            <div className="searchResults--desktop__body">
+      {size.width <= 768 ? (
+        <div className="searchResults--mobile">
+          <Tabs defaultActiveKey="1">
+            <TabPane tab={`USERS (${countUsers()})`} key="1">
               <UserList />
-            </div>
-          </div>
-
-          <div className="searchResults--desktop__items">
-            <div className="searchResults--desktop__header">
-              {`COMPANIES (${countOrganizations()})`}
-            </div>
-            <div className="searchResults--desktop__body">
+            </TabPane>
+            <TabPane tab={`COMPANIES (${countOrganizations()})`} key="2">
               <OrganizationList />
+            </TabPane>
+          </Tabs>
+        </div>
+      ) : (
+        <div className="searchResults--desktop">
+          <div className="searchResults--desktop__list">
+            <div className="searchResults--desktop__items">
+              <div className="searchResults--desktop__header">{`USERS (${countUsers()})`}</div>
+              <div className="searchResults--desktop__body">
+                <UserList />
+              </div>
+            </div>
+
+            <div className="searchResults--desktop__items">
+              <div className="searchResults--desktop__header">
+                {`COMPANIES (${countOrganizations()})`}
+              </div>
+              <div className="searchResults--desktop__body">
+                <OrganizationList />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
